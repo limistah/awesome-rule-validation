02837eb59e82b11e8ef361f59288f78965f64518
Aleem Isiaka <aleemisiaka@gmail.com>
2021-01-30 10:34:31 +0100

    Add cors support

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const cors = require("cors");
 
 const indexRouter = require("./routes/index");
 
@@ -10,6 +11,7 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors("*"));
 
 app.use("/", indexRouter);
 
